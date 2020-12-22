a3090cf9cf1598a02f2d9273d87cfd626a34ad28
Alexander <botvaozerss@gmail.com>
2020-12-22 16:42:58 +0300

    дз

--- 1.py
+++ 1.py
@@ -1,9 +1,9 @@
 list1 = ['False', True, 4.2, [23, 14]]
 
 
-def type1(tut):
-    for tut in range(len(list1)):
-        print(type(list1[tut]))
+def type1(el):
+    for el in range(len(list1)):
+        print(type(list1[el]))
     return
 
 
