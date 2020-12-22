0b8bc0b5d9b36dd1b558f9f617aa1010f2c1dd45
Alexander <botvaozerss@gmail.com>
2020-12-22 18:34:49 +0300

    дз

--- 4.py
+++ 4.py
@@ -1,3 +1,11 @@
-my_list = input('Напишите несколько склов, разделённых пробелом')
-for el in "my list":
-    print(el)
+my_str = input("Напишите несколько слов, разделённых пробелом: ")
+word = []
+count = 1
+for el in range(my_str.count(' ') + 1):
+    word = my_str.split()
+    if len(str(word)) <= 10:
+        print(f" {count}. {word [el]}")
+        count += 1
+    else:
+        print(f" {count}. {word [el] [0:10]}")
+        count += 1
